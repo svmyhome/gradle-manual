34ef1f68a40e52fc1a01f72649e72fc1908e96f5
Vladimir Sarychev <svmyhome@mail.ru>
2024-10-08 07:57:12 +0300

    initial commit

---
+++ first/src/test/java/FirstTest.java
@@ -0,0 +1,9 @@
+public class FirstTest {
+
+    public static void main(String[] args) {
+        PrintText();
+    }
+    public static void PrintText() {
+        System.out.println("Hello world !!!");
+    }
+}
